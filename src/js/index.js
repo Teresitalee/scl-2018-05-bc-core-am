0e3ee13a63ef22940e53745b6641c89fb85f9d3b
TeresitaLee <teresita.1852@hotmail.com>
2018-05-31 11:00:40 -0400

    cambios al README

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
 /* Acá va tu código */
+
+// Escribir el resultado en el HTML
+div = document.getElementById("resultado1");
+      var cadena = "";
+    for(i = 0; i < size_mensaje; i++){
+  cadena += mensaje_cifrado[i];
+ }
+div.textContent = 'Mensaje cifrado: ' + cadena;
+  }
+
+// Escribir el resultado en el HTML
+div = document.getElementById("resultado2");
+   var cadena = "";
+   for(i = 0; i < size_mensaje; i++){
+  cadena += mensaje_descifrado[i];
+}
+div.textContent = 'Mensaje descifrado: ' + cadena;
+}
