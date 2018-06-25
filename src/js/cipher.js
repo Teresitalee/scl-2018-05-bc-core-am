b34661379dbad0804756e1e36102e57896db080e
TeresitaLee <teresita.1852@hotmail.com>
2018-06-25 11:40:36 -0400

    arreglando el cipher

--- src/js/cipher.js
+++ src/js/cipher.js
@@ -2,85 +2,86 @@ window.cipher = {
 
   // eventos del DOM
   function clearBoxes() {
-    document.querySelector('#decode').value = "";
-    document.querySelector('#encode').value = "";
+    document.querySelector('#decrypted').value = "";
+    document.querySelector('#encrypted').value = "";
     document.getElementById("chains").value = "";
     document.getElementById("key").value = 0;
 }
-function encode() {
-    let decode = document.querySelector('#decode').value.toLocaleUpperCase() || "Buen dia";
-    let encode = document.querySelector('#encode');
+function encrypt() {
+    let decrypted = document.querySelector('#decrypted').value.toLocaleUpperCase() || "ejemplo";
+    let encrypted = document.querySelector('#encrypted');
     let key = document.querySelector('#key');
     let cipherArray = new Array();
-    [...decode].forEach(char => {
-        
+    [...decrypted].forEach(char => {
+        //console.log(char.charCodeAt())
         if (char.charCodeAt() === 32) {
             cipherArray.push(32);
         } else {
             charNumber = char.charCodeAt() - 65;
-            
+            //console.log(charNumber)
             cipherChar = (charNumber + parseInt(key.value)) % 26;
-            
+            //console.log(cipherChar)
             cipherArray.push(cipherChar + 65);
         }
     })
     document.getElementById("chains").value = String.fromCharCode(...cipherArray);
-    
+    //console.log(String.fromCharCode(...cipherArray))
 }
-function decode() {
-    let decode = document.querySelector('#decode');
-    let encode = document.querySelector('#encode').value.toLocaleUpperCase();
+function decrypt() {
+    let decrypted = document.querySelector('#decrypted');
+    let encrypted = document.querySelector('#encrypted').value.toLocaleUpperCase();
     let key = document.querySelector('#key');
     let cipherArray = new Array();
-    [...encode].forEach(char => {
-       
+    [...encrypted].forEach(char => {
+        //console.log(char.charCodeAt())
         if (char.charCodeAt() === 32) {
             cipherArray.push(32);
         } else {
             charNumber = char.charCodeAt() - 65;
-            
+            //console.log(charNumber)
             cipherChar = (charNumber - parseInt(key.value));
-           
+            //console.log(cipherChar)
             if (cipherChar > 25 || cipherChar < 0) cipherChar = cipherChar + 26
             cipherChar = cipherChar % 26
             cipherArray.push(cipherChar + 65);
         }
     })
     document.getElementById("chains").value = String.fromCharCode(...cipherArray);
-   
+    //console.log(String.fromCharCode(...cipherArray))
 }
 function getKey() {
-    let decode = document.querySelector('#decode').value.toLocaleUpperCase();
-    let encode = document.querySelector('#encode').value.toLocaleUpperCase();
+    let decrypted = document.querySelector('#decrypted').value.toLocaleUpperCase();
+    let encrypted = document.querySelector('#encrypted').value.toLocaleUpperCase();
     let cipherArray = new Array();
     document.getElementById("chains").value = "";
     for (let i = 0; i < 26; i++) {
         cipherArray.length = 0;
-        [...encode].forEach(char => {
-            
+        [...encrypted].forEach(char => {
+            //console.log(char.charCodeAt())
             if (char.charCodeAt() === 32) {
                 cipherArray.push(32);
             } else {
                 charNumber = char.charCodeAt() - 65;
-                
+                //console.log(charNumber)
                 cipherChar = (charNumber - i);
-                
+                //console.log(cipherChar)
                 if (cipherChar > 25 || cipherChar < 0) cipherChar = cipherChar + 26
                 cipherChar = cipherChar % 26
                 cipherArray.push(cipherChar + 65);
             }
         })
         document.getElementById("chains").value += `Key ${i}: ${String.fromCharCode(...cipherArray)}\n`;
-      
-        if (String.fromCharCode(...cipherArray) === decode) {
+        //console.log(String.fromCharCode(...cipherArray))
+        //console.log(decrypted)
+        if (String.fromCharCode(...cipherArray) === decrypted) {
             document.getElementById("chains").value = `Key is: ${i}\n`;
-            
+            //console.log("Key is: " + i);
             break;
         }
         if (i === 25) {
             document.getElementById("chains").style.display = "block";
         }
     }
-    encode.value = String.fromCharCode(...cipherArray);
-    
+    encrypted.value = String.fromCharCode(...cipherArray);
+    //console.log(String.fromCharCode(...cipherArray))
 }
